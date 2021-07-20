b9ebbe58fa96906afdafd189525363b7834df2e1
matrix <64998320+shnider0@users.noreply.github.com>
2021-07-20 16:00:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-#DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_URL = ("postgres://hpawjshciurmie:f92732626fc3b1aa0caacdcd8144b0167f819a039fc622fec39d638d1a183f4c@ec2-3-220-222-72.compute-1.amazonaws.com:5432/dna4ogo1fb8ltL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 OWNER_ID = int(1827146773)
 
